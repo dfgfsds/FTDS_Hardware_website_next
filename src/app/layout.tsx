d34f5f2c0cebc467c2da7ca1f83cfe8d65313578
MonishR0106 <monishftsolution0106@gmail.com>
2026-01-12 15:20:46 +0530

    seo content added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,13 +9,7 @@ import WhatsAppFloatingButton from "@/components/WhatsAppFloatingButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
 
-  icons: {
-    icon: "/favicon.ico",     
-   
-  },
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
